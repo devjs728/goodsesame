7c12ce8ad87db5aca9e902d5d99a06b219a49905
devjs <yurii.js@outlook.com>
2021-08-04 13:36:15 -0700

    changePassword

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,6 +5,7 @@ import SignIn from "./page/signIn";
 import SignUp from "./page/signUp";
 import ForgotPassword from "./page/forgotPassword";
 import { AuthRoute } from "./utils/privateRoute";
+import ChangePassword from "./page/changePassword";
 
 export default function Routes() {
   return (
@@ -13,6 +14,7 @@ export default function Routes() {
       <AuthRoute exact path="/sign-up" component={SignUp} />
       <AuthRoute exact path="/confirm-email" component={ConfirmEmail} />
       <AuthRoute exact path="/forgot-password" component={ForgotPassword} />
+      <AuthRoute exact path="/change-password" component={ChangePassword} />
     </Switch>
   );
 }
---
+++ src/page/changePassword.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Auth from "../component/auth";
+import AuthTitle from "../component/auth/authTitle";
+import AuthDescription from "../component/auth/authDescription";
+import NormalInput from "../component/inputs/normalInput";
+import PasswordInput from "../component/inputs/passwordInput";
+import PinkButton from "../component/buttons.tsx/pinkButton";
+
+export default function ChangePassword() {
+  return (
+    <Auth>
+      <AuthTitle>Changer de mot de passe</AuthTitle>
+      <div className="mt-3">
+        <AuthDescription>
+          Mot de passe oublié ?<br></br>On t’explique par e-mail comment le
+          changer.
+        </AuthDescription>
+      </div>
+      <div className="mt-6">
+        <div>
+          <NormalInput label="Code de réinitialisation:" type="text" />
+        </div>
+        <div className="mt-6">
+          <PasswordInput label="Nouveau mot de passe :" />
+        </div>
+        <div className="mt-6">
+          <PasswordInput label="Répétez le mot de passe" />
+        </div>
+        <div className="mt-6">
+          <PinkButton>Me connecter</PinkButton>
+        </div>
+      </div>
+    </Auth>
+  );
+}
